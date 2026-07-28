a2066589e2d1162f649948c97600e2344f3c5578
Steffen Beisenherz <git@beisenherz.dev>
2026-07-28 19:16:38 +0200

    rework renovate presets

--- scripts/gen-default.mjs
+++ scripts/gen-default.mjs
@@ -3,7 +3,7 @@
 import { readdirSync, writeFileSync } from "node:fs";
 
 const REPO = "sironheart/renovate-preset";
-const DIRS = ["config", "managers", "policies", "versioning"];
+const DIRS = ["config", "policies", "versioning"];
 
 const extendsList = DIRS.flatMap((dir) =>
   readdirSync(dir)
